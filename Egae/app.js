1f13c3c54bdf038422035509014aa48e6aa7d6de
David ADOLPHE <dadoo97441@gmail.com>
2020-10-28 10:37:17 +0400

    début overlay à droite

--- Egae/app.js
+++
@@ -1,6 +0,0 @@
-const hidden = document.querySelector(".bottom-hidden");
-const colo = document.querySelector(".colonne");
-
-colo.addEventListener("mouseover", function () {
-    hidden.classList.add("slide-top")
-})
